5806f209166e6b5398886e25904f4805b5f2112a
Jonathan <yin.jon10@gmail.com>
2014-05-21 14:42:13 -0400

    THIS IS STUFF

---
+++ HeapSort.java
@@ -0,0 +1,13 @@
+public class HeapSort{
+    public int[] heapify(int[] data){
+	if (data.length == 0 || data.length == 1){
+	    return data;
+	}
+	for (int i = 0; i < data.length; i++){
+	}}
+    public void swap(int[] nums, index one, index two){
+	int a = nums[one];
+	int b = nums[two];
+	nums[one] = b;
+	nums[two] = a;
+    }
